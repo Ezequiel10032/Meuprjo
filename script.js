7b7b94e30067113a69430912f61bed91ed9fd240
Ezequiel1020100 <ezequielbritojesus@gmail.com>
2024-09-19 17:36:34 -0300

    update

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
 //Variáveis//
-var nome = prompt("  Por favor Digite Seu Nome");
-var foguete = document.querySelector(".bi-rocket-takeoff");
+//var nome = prompt("  Por favor Digite Seu Nome");
+var foguete1 = document.querySelector("#foguete1");
+var foguete2= document.querySelector("#foguete2")
 var começar = document.getElementById("btniniciar")
+var tempo = document.getElementById("tempo")
+//ativação 
+foguete2.addEventListener("click", () =>
+iniciar());
+function iniciar() {
+  foguete1.style.animationDuration = "4s";
+  segundos=16
+  
+} 
+
+//parar
+foguete1.addEventListener("click", () =>
+parar());
+function parar() {
+  foguete1.style.animationDuration = "0s";
+} 
 
 
-foguete.addEventListener("click", () => Voar());
-function voar() {
-  foguete.style.animationDuration = "4s";
-  tempo.style.display = "none";
-}
-foguete.addEventListener("click", () => aviso());
-function aviso() {
-  alert(
-    `${nome} VOCÊ CONSEGUIU CONCLUIR O NIVEL 1 EM ${segundos} SEGUNDOS. PARABÉNS,ZEKA DESEJA BOA SORTE;`
-  );
-}
-começar.addEventListener("click", () => inicio());
-function inicio() {
-  foguete.style.animationDuration = "0.3s";
-  tempo.style.display = "inline";
-  segundos = 0;
-}
+foguete2.addEventListener('click', () => conometro()); // Sem parênteses para não chamar a função imediatamente
 
-começar.addEventListener("click", () => mostrar());
-var segundos = 0;
 function conometro() {
-  segundos++;
-  document.getElementById("tempo").innerHTML = segundos;
+    if (segundos > 0) {
+        segundos--;
+        tempo.innerHTML = segundos; // Atualizando o valor no HTML
+    } else {
+        fim(); // Chamando a função fim quando segundos é 0
+    }
 }
 setInterval(function () {
-  conometro();
+    conometro();
 }, 1000);
 
+function fim() {
+foguete1.style.animationDuration = "0s";
+foguete2.style.display="block"
+foguete2.style.color="blue"
+}
 
+foguete2.addEventListener('click', () => sumir());
+function sumir(){
+foguete2.style.display="none"
+}
 
