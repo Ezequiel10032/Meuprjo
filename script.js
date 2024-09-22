0fafad99be6798c2d15a0170bfc731b61bfa763a
Ezequiel1020100 <ezequielbritojesus@gmail.com>
2024-09-22 12:13:13 -0300

    update

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ var começar = document.getElementById("btniniciar")
 var tempo = document.getElementById
 ("tempo")
 var msg1= document.getElementById("txt2")
-var rocket = document.getElementById("rocket")
-
+var rocket = document.getElementById("rocket");
+var pontuação = document.getElementById("pontos")
 //Inicialização
 
 alert("Olá, Ezequiel deseja boas vindas. Clique em Super Ezq para Começar!")
@@ -28,29 +28,25 @@ rocket.style.visibility ="hidden"
 }
 
 
-//ativação 
+//mudanças após ativação 
 btnliga.addEventListener("click", () =>
 iniciar());
 function iniciar() {
   foguete1.style.animationDuration = "4s";
-  segundos=16
+  segundos=21
   msg1.style.color="red"
     msg1.style.textDecoration = "line-through";
-} 
+números = 1
+}
+btnliga.addEventListener('click', () => sumir());
+function sumir(){
+btnliga.style.display="none"
+}
 
-
-//parar
-foguete1.addEventListener("click", () =>
-parar());
-function parar() {
-  foguete1.style.animationDuration = "0s";
-} 
-
-
-btnliga.addEventListener('click', () => conometro()); // Sem parênteses para não chamar a função imediatamente
-
+// sistema de tempo 
+btnliga.addEventListener('click', () => conometro()); 
 function conometro() {
-    if (segundos > 0) {
+    if (segundos >0) {
         segundos--;
         tempo.innerHTML = segundos; // Atualizando o valor no HTML
     } else {
@@ -61,16 +57,35 @@ setInterval(function () {
     conometro();
 }, 1000);
 
+//sistema de pontos 
+foguete1.addEventListener("click", () =>
+pontos());
+
+function pontos(){
+
+if (segundos>0) {
+        números++;
+     pontuação.innerHTML= números 
+}
+}
+setInterval(function () {
+    pontos();
+}, 100);
+
+//sistema de recorde
+ 
+  
+
+
+//sistema de quando acabar o tempo 
 function fim() {
 foguete1.style.animationDuration = "0s";
 btnliga.style.display="block"
 btnliga.style.color="blue"
 msg1.style.color="greenyellow"
-    msg1.style.textDecoration = "none";
+msg1.style.textDecoration = "none";
+
+
 }
 
-btnliga.addEventListener('click', () => sumir());
-function sumir(){
-btnliga.style.display="none"
-}
 
